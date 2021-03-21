5b4e9b8ea10ffbf59ac9eef925fca612db49cab8
Mohamed Dawood <mo.ah.dawood@gmail.com>
2021-03-21 05:21:16 +0200

    ....

--- lib/src/context_extension.dart
+++ lib/src/context_extension.dart
@@ -16,4 +16,55 @@ extension ContextExtentions on BuildContext {
     }
     return 0;
   }
+
+  /// get bodyText1 property from text theme
+  TextStyle? get bodyText1 => Theme.of(this).textTheme.bodyText1;
+
+  /// get bodyText2 property from text theme
+
+  TextStyle? get bodyText2 => Theme.of(this).textTheme.bodyText2;
+
+  /// get button property from text theme
+
+  TextStyle? get button => Theme.of(this).textTheme.button;
+
+  /// get caption property from text theme
+
+  TextStyle? get caption => Theme.of(this).textTheme.caption;
+
+  /// get headline1 property from text theme
+
+  TextStyle? get headline1 => Theme.of(this).textTheme.headline1;
+
+  /// get headline2 property from text theme
+
+  TextStyle? get headline2 => Theme.of(this).textTheme.headline2;
+
+  /// get headline3 property from text theme
+
+  TextStyle? get headline3 => Theme.of(this).textTheme.headline3;
+
+  /// get headline4 property from text theme
+
+  TextStyle? get headline4 => Theme.of(this).textTheme.headline4;
+
+  /// get headline5 property from text theme
+
+  TextStyle? get headline5 => Theme.of(this).textTheme.headline5;
+
+  /// get headline6 property from text theme
+
+  TextStyle? get headline6 => Theme.of(this).textTheme.headline6;
+
+  /// get overline property from text theme
+
+  TextStyle? get overline => Theme.of(this).textTheme.overline;
+
+  /// get subtitle1 property from text theme
+
+  TextStyle? get subtitle1 => Theme.of(this).textTheme.subtitle1;
+
+  /// get subtitle2 property from text theme
+
+  TextStyle? get subtitle2 => Theme.of(this).textTheme.subtitle2;
 }
