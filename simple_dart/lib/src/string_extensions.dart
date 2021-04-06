30cce408c5b1398b1b1b004f7c8c0801512e164a
Mohamed Dawood <mo.ah.dawood@gmail.com>
2021-04-06 20:32:33 +0200

    social

--- simple_dart/lib/src/string_extensions.dart
+++ simple_dart/lib/src/string_extensions.dart
@@ -43,19 +43,23 @@ extension StringExtensions on String? {
   bool get isNullOrEmpty {
     return this == null || this == '';
   }
+  // check string is locale egyptian phone
 
   bool get isLocaleEgyptianPhone {
     return RegExp(r'^(01|1)([0-9]{9})$').hasMatch(this ?? '');
   }
+  // check string is international egyptian phone
 
   bool get isInternationalEgyptianPhone {
     return RegExp(r'^(00201|201|\+201)([0-9]{9})$').hasMatch(this ?? '');
   }
+  // check string is locale ksa phone
 
   bool get isLocaleKsaPhone {
     return RegExp(r'^(05)(5|0|3|6|4|9|1|8|7)([0-9]{7})$').hasMatch(this ?? '');
   }
 
+  // check string is international ksa phone
   bool get isInternationalKsaPhone {
     return RegExp(
             r'^(009665|0096605|9665|96605|\+9665|\+96605|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$')
@@ -116,4 +120,47 @@ extension StringExtensions on String? {
     if (this != null) return int.tryParse(this!);
     return null;
   }
+
+  // check string is gitHub valid url
+  // expression are fro  herehttps://github.com/nothingrandom/social-regex
+  bool get isGitHubUser => RegExp(
+          r'^(http(s)?:\/\/)?(www\.)?github\.com\/(?!-)(?:[A-z0-9-]){1,39}[^-]\/?$')
+      .hasMatch(this ?? '');
+  // check string is gitHub valid url
+  // expression are fro  here https://github.com/nothingrandom/social-regex
+  bool get isRedditUser => RegExp(
+          r'^(http(s)?:\/\/)?(www\.)?reddit\.com\/user\/(?:[A-Za-z0-9_-]{3,20})\/?$')
+      .hasMatch(this ?? '');
+
+  // check string is instagram valid url
+  // expression are fro  here https://github.com/nothingrandom/social-regex
+  bool get isInstagramUser => RegExp(
+          r'^(http(s)?:\/\/)?(www\.)?instagram\.com\/([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)\/?$')
+      .hasMatch(this ?? '');
+
+  // check string is linkedin valid url
+  // expression are fro  here https://github.com/nothingrandom/social-regex
+  bool get isLinkedinUser => RegExp(
+          r'^(http(s)?:\/\/)?(www\.)?linkedin\.com\/(in|profile|pub)\/([A-z 0-9 _ -]+)\/?$')
+      .hasMatch(this ?? '');
+
+  // check string is facebook valid url
+  // expression are fro  here https://github.com/nothingrandom/social-regex
+  bool get isTwitterUser =>
+      RegExp(r'^(http(s)?:\/\/)?(www\.)?twitter\.com\/[A-z 0-9 _]{1,15}\/?$')
+          .hasMatch(this ?? '');
+
+  // check string is youtube valid url
+  bool get isYoutubeUrl =>
+      RegExp(r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$')
+          .hasMatch(this ?? '');
+  // check string is youtube valid url
+  bool get isFacbookPageOrProfile => RegExp(
+          r'/(?:https?:\/\/)?(?:www\.)?facebook\.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w\-]*\/)*?(\/)?([\w\-\.]{5,})/ig')
+      .hasMatch(this ?? '');
+
+  // check string is snapchat valid url
+  bool get isSnapchatProfile => RegExp(
+          r'(?:https?:)?\/\/(?:www\.)?snapchat\.com\/add\/([A-z0-9\.\_\-]+)\/?')
+      .hasMatch(this ?? '');
 }
