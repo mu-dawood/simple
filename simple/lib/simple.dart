742a08839208cdda830cf35e6b3e2e3ee3d95f06
Mohamed dawood <mo.ah.dawood@gmail.com>
2022-02-19 22:24:35 +0200

    ...

--- simple/lib/simple.dart
+++ simple/lib/simple.dart
@@ -7,5 +7,6 @@ export 'src/context_extension.dart';
 export 'src/date_time_extensions.dart';
 export 'src/duration_extension/duration_extension.dart';
 export 'src/gen_l10n/simple_localizations.dart';
+export 'src/snackbar_extension.dart';
 export 'src/string_extensions.dart';
 export 'src/validations/validations.dart';
